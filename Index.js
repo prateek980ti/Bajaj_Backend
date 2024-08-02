b064cccc2324b80738ca2a15e2f723b17011b61e
prateek980ti <pdawalia@gmail.com>
2024-08-02 20:36:49 +0530

    Add Vercel

--- Index.js
+++ Index.js
@@ -33,6 +33,6 @@ app.post('/bfhl', (req, res) => {
     });
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+module.exports=(req,res)=>{
+    app(req,res);
+}
